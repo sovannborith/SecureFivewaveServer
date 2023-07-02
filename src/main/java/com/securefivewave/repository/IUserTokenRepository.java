54d0f2d79153146ed77901f9960dc8c8354e29d6
Sovannborith <yun.sovannborith@gmail.com>
2023-07-02 16:45:10 +0700

    Refresh Token

--- src/main/java/com/securefivewave/repository/IUserTokenRepository.java
+++ src/main/java/com/securefivewave/repository/IUserTokenRepository.java
@@ -13,11 +13,12 @@ public interface IUserTokenRepository extends ListCrudRepository<UserToken, Long
 
 	@Query(value = "SELECT U.* from TBL_USER_TOKEN U WHERE USER_ID=:userId", nativeQuery = true)
 	List<UserToken> getUserTokenByUserId(@Param("userId") Long userId);
+	
 	@Query(value = "SELECT U.* from TBL_USER_TOKEN U WHERE U.ACCESS_TOKEN=:token", nativeQuery=true)
 	UserToken getUserTokenByToken(@Param("token") String token);
 
-	@Query(value = "SELECT U.* from TBL_USER_TOKEN U WHERE U.USER_ID=:userId AND IS_EXPIRED=FALSE AND IS_REVOKED=FALSE", nativeQuery=true)
-	List<UserToken> getAllValidUserTokenByUserId(Long userId);
+	@Query(value = "SELECT U.* from TBL_USER_TOKEN U WHERE U.USER_ID=:userId AND IS_EXPIRED=0 AND IS_REVOKED=0", nativeQuery=true)
+	List<UserToken> getAllValidUserTokenByUserId(@Param("userId") Long userId);
 	/* @Query(value = "SELECT U.* from TBL_USER_ U WHERE USER_ID=:userId AND ROLE_ID=:roleId", nativeQuery=true)
 	UserToken getUserTokenById(@Param("id") Long id); */
 }
